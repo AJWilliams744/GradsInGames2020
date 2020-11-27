e5f3b5aeff9a20641d6bf7cfaf888aeeec0aaa7e
Alex Williams <alex.williams@or3d.co.uk>
2020-11-27 00:35:39 +0000

    Toon shader improved, fire shader added, Dimension  Dark music added

--- Assets/Scripts/Enums.cs
+++ Assets/Scripts/Enums.cs
@@ -3,6 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public enum TravelInstructions { Travel, Stay, nothing}
-public enum TravelLocations { Home, Dimension45, DimensionBlue}
+public enum TravelLocations { Home, DarkDimension, DimensionBlue}
 
 public enum WorldChoiceTriggers { Choice1, Choice2, Choice3, Choice4}
---
+++ Assets/Scripts/Enviroment/Fire.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Fire : MonoBehaviour
+{
+    [SerializeField] private float minFireBrightness = 40;
+    [SerializeField] private float maxFireBrightness = 80;
+    [SerializeField] private float flickerSpeed = 0f;
+
+    [SerializeField] private Light flameLight;
+
+    private void Start()
+    {
+        flameLight.intensity = maxFireBrightness;
+        if(flickerSpeed != 0)
+        {
+            StartCoroutine(Flicker());
+        }
+        
+    }
+
+    private IEnumerator Flicker()
+    {
+        float midSpeed = flickerSpeed / 2;
+        while (true)
+        {            
+            for (float i = 0; i < midSpeed; i += Time.deltaTime)
+            {
+                flameLight.intensity = Mathf.Lerp(maxFireBrightness, minFireBrightness, i / midSpeed);
+                yield return new WaitForEndOfFrame();
+            }
+            flameLight.intensity = minFireBrightness;
+            for (float i = 0; i < midSpeed; i += Time.deltaTime)
+            {
+                flameLight.intensity = Mathf.Lerp(minFireBrightness, maxFireBrightness, i / midSpeed);
+                yield return new WaitForEndOfFrame();
+            }
+        }
+
+    }
+
+}
